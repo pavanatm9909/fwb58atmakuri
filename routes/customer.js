b652caf40efe801fda32177b3364db7f42819ddd
pavanatm9909 <S545458@NWMISSOURI.EDU>
2021-11-03 13:18:16 -0500

    added logic for part 5

--- routes/customer.js
+++ routes/customer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('customer', { title: 'Search Results' });
+  res.render('customer', { title: 'Search Results Customer' });
 });
 
 module.exports = router;
